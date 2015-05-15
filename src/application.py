1f08cb881f3420a547980be444e1a644d7afec8d
Matt Seymour <matt@mattseymour.net>
2015-05-15 13:41:12 +0100

    Application code and requirements
    Install requirements using `pip install -r requirements.txt`

---
+++ src/application.py
@@ -0,0 +1,55 @@
+# Python 2,3 support
+from __future__ import print_function
+from six.moves import input
+
+import os
+import sys
+import re
+import requests
+from bs4 import BeautifulSoup
+
+def get(a):
+    # Performs a http request, returning a string
+    response = requests.get(a)
+    return response.text
+
+def element_count(a, b):
+    # element count
+    output = get(a)
+
+    soup = BeautifulSoup(output)
+
+    return len(soup.find_all(b))
+
+def fizz_or_buzz(number):
+    # Return fizz or buzz or fizzbuzz
+    # number/3 then fizz
+    # number/5 then buzz
+    # number/(3,5) then fizz buzz
+    # number !/ (3,5) then <empty>
+    is_three = True if number % 3 == 0 else False
+    is_five = True if number % 5 == 0 else False
+
+    if is_three:
+        return 'fizz'
+    elif is_five:
+        return 'buzz'
+    elif is_three and is_five:
+        return 'fizzbuzz'
+    else:
+        return ''
+
+import sys
+if __name__ == '__main__':
+        # application will take two args url, html tag type (a, ul, div, ...etc)
+        if len(sys.argv) < 3:
+            url = input('Url: ')
+            tag = input('Tag: ')
+        else:
+            url = sys.argv[1]
+            tag = sys.argv[2]
+
+        count = element_count(url, tag)
+        fizzBuzz = fizz_or_buzz(count)
+        print('{} = {} = {}'.format(tag, count, fizzBuzz))
+
